50c46d52aa303aeb4cf6948e55fc81ec23c04385
firebee92 <53724949+firebee92@users.noreply.github.com>
2020-12-26 17:00:08 +0100

    Update Simulation.R

--- R/Simulation.R
+++ R/Simulation.R
@@ -1,12 +1,14 @@
 
 # Time complexity simulation for change in mean:
-one.simu <- function(n, type = "sample", func = "OP", beta = 1)
+mean.simu <- function(n, m, type = "sample", func = "OP", beta = 1)
 {
   changetype = 'mean'
-  m = sample(n/10)
-  if(type == "sample"){v <- sample(m)}else{v <- m:1}
-  w = sample(m)
-  x=rep(v,w*n/sum(w))+runif(length(rep(v,w*n/sum(w))))
+  if(type == "sample"){v1 <- sample(m+1)}else{v <- (m+1):1}
+  a = floor((n-30)/60)
+  v = sample(1:a, m, replace = FALSE) - runif(m)/2
+  cps = sort(round(v*60, 0))
+  ngroup = diff(c(0,cps,n))
+  x=unlist(lapply(ngroup, function(x) rnorm(x,0,1)))/4+rep(v1,ngroup)
   f=get(func)
   t <- system.time(f(x, beta, changetype='mean'))[[1]]
   return(t)
@@ -23,6 +25,6 @@ var.simu <- function(n, m, func = "OP", beta = 1)
   ngroup = diff(c(0,cps,n))
   x=unlist(lapply(ngroup, function(x) rnorm(x,0,rlnorm(1,0,log(10)/2))))
   f=get(func)
-  t <- system.time(f(x, beta, changetype='mean'))[[1]]
+  t <- system.time(f(x, beta, changetype='meanvar'))[[1]]
   return(t)
 }
